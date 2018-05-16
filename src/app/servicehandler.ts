300644f013a81df521a0cd5294112be7ead1ebd9
Mihir Sasmal <mihirsasmal@gmail.com>
2018-05-17 00:24:56 +0530

    cors issue handled by redirecting the path through a proxy server

--- src/app/servicehandler.ts
+++ src/app/servicehandler.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
-
 const jiraRestEndPoint = "/jira/rest/api/2/";
 @Injectable()
 export class ServiceHandler {
@@ -13,12 +12,13 @@ export class ServiceHandler {
 
     searchJql(url: string, jql: string) {
         this.httpOptions = {
-            headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Basic ' + btoa(this.username + ':' + this.password), 'X-Atlassian-Token': 'no-check' ,'Access-Control-Allow-Origin' :'*' })
-        };
-
-        let jqlendpoint = url + jiraRestEndPoint + "search?jql=" + jql;
+            headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Basic ' + btoa(this.username + ':' + this.password), 'X-Atlassian-Token': 'no-check'})
+        };        
+               
+        let jqlendpoint = "/proxy/?url="+ url + jiraRestEndPoint + "search?jql=" + jql;
 
         return this.http.get(jqlendpoint, this.httpOptions);
+
     }
 
 
